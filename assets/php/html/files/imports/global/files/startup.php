7a03002ad3ddd95bbeb7b75b219c716355055099
Zach Vaughan <FusedKush@gmail.com>
2019-03-01 13:29:29 -0700

    Local resources now update properly

--- assets/php/html/files/imports/global/files/startup.php
+++ assets/php/html/files/imports/global/files/startup.php
@@ -1,2 +1,2 @@
 <script src="/assets/scripts/min/s/serverVersion.min.js?t=<?php echo time(); ?>" async></script>
-<script src="/assets/scripts/min/s/loader.min.js?v=1" async></script>
+<script src="/assets/scripts/min/s/loader.min.js?v=1.2" async></script>
