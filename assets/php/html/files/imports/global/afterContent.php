1786366aedf8318dd8da7cc10af8f614ef5e7d60
Zach Vaughan <FusedKush@gmail.com>
2019-07-14 17:13:47 -0600

    Remove development imports

--- assets/php/html/files/imports/global/afterContent.php
+++ assets/php/html/files/imports/global/afterContent.php
@@ -3,7 +3,7 @@
 <!-- Alert Popups -->
 <?php include_once("files/afterContent/alert-popup.php"); ?>
 <!-- Form Templates -->
-<?php include_once("files/afterContent/form-footer-template.php"); ?>
+<?php // include_once("files/afterContent/form-footer-template.php"); ?>
 <!-- Templates -->
 <?php include_once("files/afterContent/templates.php"); ?>
 <!-- No Javascript Error -->
