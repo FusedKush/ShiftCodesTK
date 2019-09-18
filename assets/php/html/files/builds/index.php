46e64934dfc67fa5dde5c4ac69aa0453aaafea49
Zach Vaughan <FusedKush@gmail.com>
2019-09-17 18:02:49 -0600

    Rearranged titles

--- assets/php/html/files/builds/index.php
+++ assets/php/html/files/builds/index.php
@@ -47,6 +47,11 @@
             <h2 class="string">SHiFT Codes for&nbsp;<span class="selected chosen bl1">Borderlands: GOTY</span></h2>
             <div class="link-container">
               <a
+                class="button bl3"
+                href="/bl3"
+                data-string="Borderlands 3"
+                data-quote="Lets make some mayhem.">BL3</a>
+              <a
                 class="button bl1"
                 href="/bl1"
                 data-string="Borderlands: GOTY"
@@ -63,11 +68,6 @@
                 data-string="Borderlands: TPS"
                 data-long-string="Borderlands: The Pre-Sequel"
                 data-quote="Come to the moon, hunt a vault, be a hero.">TPS</a>
-              <a
-                class="button bl3"
-                href="/bl3"
-                data-string="Borderlands 3"
-                data-quote="Lets make some mayhem.">BL3</a>
             </div>
           </div>
         </div>
