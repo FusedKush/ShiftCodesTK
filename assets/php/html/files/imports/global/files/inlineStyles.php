72e1b385addbaf2faa7571b047535756ed34af64
Zach Vaughan <FusedKush@gmail.com>
2019-10-16 20:17:38 -0600

    Styles adjustments to the landing page

--- assets/php/html/files/imports/global/files/inlineStyles.php
+++ assets/php/html/files/imports/global/files/inlineStyles.php
@@ -1,11 +1,11 @@
 <style id="startup">
   @media screen and (max-width: 474px) {
-    header.main .intro   { height: calc(30vh + 32px); }
-    main:not(.no-header) { min-height: calc(70vh - 32px); }
+    header.main .intro   { height: 30vh; }
+    main:not(.no-header) { min-height: 70vh; }
   }
   @media screen and (min-width: 475px) {
     header.main .intro   { height: calc(30vh + 64px); }
-    main:not(.no-header) { min-height: calc(70%- 64px); }
+    main:not(.no-header) { min-height: calc(70vh- 64px); }
   }
   body {
     background-color: #0f1e2d;
