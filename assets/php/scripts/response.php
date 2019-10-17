6576445711543556ef07ab61a1e2d0bb498134b0
Zach Vaughan <FusedKush@gmail.com>
2019-10-16 20:45:28 -0600

    Additional functionality for AJAX Responses

--- assets/php/scripts/response.php
+++ assets/php/scripts/response.php
@@ -1,21 +1,47 @@
 <?php
   class Response {
     public $statusCode = 0;
+    public $payload = [];
+    public $warnings = [];
+    public $errors = [];
 
+    private function addArray($array, $content, $name) {
+      $p = &$this->$array;
+
+      if ($name !== null) {
+        $p[$name] = $content;
+        return $p[$name];
+      }
+      else {
+        $len = count($p);
+
+        $p[$len] = $content;
+        return $p[$len];
+      }
+    }
     public function set($statusCode) {
-      $this->statusCode = $statusCode;
+      return $this->statusCode = $statusCode;
     }
-    public function addPayload($content) {
-      $this->payload[] = $content;
+    public function addPayload($content, $name = null) {
+      return $this->addArray('payload', $content, $name);
     }
-    public function addWarning($content) {
-      $this->warnings[] = $content;
+    public function addWarning($content, $name = null) {
+      return $this->addArray('warnings', $content, $name);
     }
-    public function addError($content) {
-      $this->errors[] = $content;
+    public function addError($content, $name = null) {
+      return $this->addArray('errors', $content, $name);
+    }
+    public function fatalError($statusCode, $content) {
+      $this->set($statusCode);
+      $this->addError($content);
+      $this->send();
+    }
+    public function push() {
+      echo json_encode($this);
     }
     public function send() {
-      echo json_encode($this);
+      $this->push();
+      exit();
     }
   }
 ?>
