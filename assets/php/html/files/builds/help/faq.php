a47d47d69aa6d65348612d1920f807b32857a197
Zach Vaughan <FusedKush@gmail.com>
2019-10-21 21:46:25 -0600

    Updated markup for dropdown panel changes made by b8aa6dd

--- assets/php/html/files/builds/help/faq.php
+++ assets/php/html/files/builds/help/faq.php
@@ -39,7 +39,7 @@
     <!-- Main Content -->
     <main class="content-wrapper">
       <header class="toc dropdown-panel">
-        <button class="header">
+        <button class="header dropdown-panel-toggle">
           <div class="wrapper">
             <div class="title">
               <div class="icon">
@@ -61,219 +61,63 @@
         <header>
           <h2 class="title">SHiFT</h2>
         </header>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">What is SHiFT?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>SHiFT is a service created by Gearbox to reward their players with in-game loot and special events.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">What is SHiFT?</h3>
+          <div class="body"><p>SHiFT is a service created by Gearbox to reward their players with in-game loot and special events.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Do you need an account to use SHiFT?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>Yes. You can create a free account&nbsp;<a class="themed" href="https://shift.gearboxsoftware.com/registration/pre" rel="external noopener" target="_blank" title="Register for SHiFT (External Link)" aria-label="Register for SHiFT (External Link)"><span class="fas fa-external-link-square-alt" title="External Link" aria-label="External Link">&nbsp;</span><span class="label">here</span></a>.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Do you need an account to use SHiFT?</h3>
+          <div class="body"><p>Yes. You can create a free account&nbsp;<a class="themed" href="https://shift.gearboxsoftware.com/registration/pre" rel="external noopener" target="_blank" title="Register for SHiFT (External Link)" aria-label="Register for SHiFT (External Link)"><span class="fas fa-external-link-square-alt" title="External Link" aria-label="External Link">&nbsp;</span><span class="label">here</span></a>.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">How old do you need to be to use SHiFT?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>You must be at least 18 years old to sign up for SHiFT.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">How old do you need to be to use SHiFT?</h3>
+          <div class="body"><p>You must be at least 18 years old to sign up for SHiFT.</p></div>
         </div>
       </section>
       <section class="faq-group">
         <header>
           <h2 class="title">ShiftCodesTK</h2>
         </header>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Is ShiftCodesTK affiliated with Gearbox or 2K?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>Nope, ShiftCodesTK is a completely fan-made service, and is not affiliated with Gearbox Software or 2K Games in any way.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Is ShiftCodesTK affiliated with Gearbox or 2K?</h3>
+          <div class="body"><p>Nope, ShiftCodesTK is a completely fan-made service, and is not affiliated with Gearbox Software or 2K Games in any way.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Can you create a SHiFT Code for me? Can you change the properties of a SHiFT Code? Can you give me a legendary?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>No, we have no control over the SHiFT Codes themselves, and just share the codes released by Gearbox.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Can you create a SHiFT Code for me? Can you change the properties of a SHiFT Code? Can you give me a legendary?</h3>
+          <div class="body"><p>No, we have no control over the SHiFT Codes themselves, and just share the codes released by Gearbox.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Why is ShiftCodesTK not updated as soon as SHiFT Codes are posted?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>At the time of writing, ShiftCodesTK does not have access to the necessary features of the Twitter & Facebook APIs to retrieve the necessary posts and automatically parse them, as well as lacking the ability to then update our own personal pages once this information has been obtained.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Why is ShiftCodesTK not updated as soon as SHiFT Codes are posted?</h3>
+          <div class="body"><p>At the time of writing, ShiftCodesTK does not have access to the necessary features of the Twitter & Facebook APIs to retrieve the necessary posts and automatically parse them, as well as lacking the ability to then update our own personal pages once this information has been obtained.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Do you have any information on future titles, expansions, or events related to Borderlands?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>Nope, we have no information on anything regarding the future of Borderlands.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Do you have any information on future titles, expansions, or events related to Borderlands?</h3>
+          <div class="body"><p>Nope, we have no information on anything regarding the future of Borderlands.</p></div>
         </div>
       </section>
       <section class="faq-group">
         <header>
           <h2 class="title">SHiFT Codes</h2>
         </header>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">What are SHiFT Codes?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>SHiFT Codes are 25-character keys that grant in-game rewards.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">What are SHiFT Codes?</h3>
+          <div class="body"><p>SHiFT Codes are 25-character keys that grant in-game rewards.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Where are SHiFT Codes released?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>SHiFT Codes are typically released on Twitter and Facebook via official handles, but are also sometimes released during special events and livestreams.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Where are SHiFT Codes released?</h3>
+          <div class="body"><p>SHiFT Codes are typically released on Twitter and Facebook via official handles, but are also sometimes released during special events and livestreams.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">How often are SHiFT Codes released?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>SHiFT Codes are typically released every Friday around 10AM PST.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">How often are SHiFT Codes released?</h3>
+          <div class="body"><p>SHiFT Codes are typically released every Friday around 10AM PST.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">Do SHiFT Codes ever expire?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>Yes, most SHiFT Codes will be issued with an Expiration Date, typically about two to three weeks after their release. However, some SHiFT Codes have and can be issued without an Expiration Date.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">How many times can a SHiFT Code be redeemed?</h3>
+          <div class="body"><p>SHiFT Codes can be redeemed once per platform, per account.</p></div>
         </div>
-        <div class="dropdown-panel">
-          <button class="header">
-            <div class="wrapper">
-              <div class="title">
-                <div class="string">
-                  <h3 class="primary">How many times can a SHiFT Code be redeemed?</h3>
-                </div>
-              </div>
-              <div class="indicator">
-                <span class="fas fa-chevron-right"></span>
-              </div>
-            </div>
-          </button>
-          <div class="body content-container">
-            <p>SHiFT Codes can be redeemed once per account.</p>
-          </div>
+        <div class="dropdown-panel c">
+          <h3 class="primary">Do SHiFT Codes ever expire?</h3>
+          <div class="body"><p>Yes, most SHiFT Codes will be issued with an Expiration Date, typically about two to three weeks after their release. However, some SHiFT Codes have and can be issued without an Expiration Date.</p></div>
         </div>
         <div class="dropdown-panel c">
           <div class="primary">What games support SHiFT Codes?</div>
