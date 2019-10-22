dca7eee586a2f6224f2a5c53af8b64fcb9e6d5fe
Zach Vaughan <FusedKush@gmail.com>
2019-10-21 20:26:50 -0600

    Updated "Updates" page to support pagination

--- assets/php/scripts/getChangelogs.php
+++ assets/php/scripts/getChangelogs.php
@@ -1,45 +1,162 @@
 <?php
-// Database Credentials
-require_once('dbConfig.php');
+  // Database
+  require_once('dbConfig.php');
+  // Response
+  require_once('response.php');
+  $response = new Response;
+  $params = (function () use (&$response) {
+    $defaultVals = [
+      'limit'    => 10,
+      'offset'   => 0,
+      'hash'     => false,
+      'firstRun' => false
+    ];
+    $arr = [];
 
-class Response {
-  public $response = array();
-}
-class Update {
-  public $version;
-  public $date;
-  public $type;
-  public $notes;
-}
+    foreach ($defaultVals as $key => $defaultVal) {
+      if (array_key_exists($key, $_GET)) {
+        $val = $_GET[$key];
+        $success = function () use (&$arr, $key, $val) {
+          $arr[$key] = htmlspecialchars($val);
+        };
+        $fail = function () use (&$arr, &$response, $defaultVals, $key, $val) {
+          $response->addWarning([
+            'name'      => 'Illegal Parameter Value',
+            'parameter' => $key,
+            'value'     => $val
+          ]);
+          $arr[$key] = $defaultVals[$key];
+        };
 
-$response = new Response;
-$info = array('version', 'date', 'type', 'notes');
+        if ($key == 'firstRun') {
+          if ($val == 'true' || $val == 'false') {
+            $success();
+          }
+          else {
+            $fail();
+          }
+        }
+        else if ($key == 'limit' || $key == 'offset') {
+          if (is_numeric($val)) {
+            $success();
+          }
+          else {
+            $fail();
+          }
+        }
+        else if ($key == 'hash') {
+          if ($val == 'false') {
+            $arr[$key] = false;
+          }
+          else {
+            $success();
+          }
+        }
+      }
+      else {
+        $response->addWarning([
+          'name'           => 'Missing Parameter',
+          'parameter'      => $key,
+          'inheritedValue' => $defaultVal
+        ]);
+        $arr[$key] = $defaultVal;
+      }
+    }
 
-// SQL Preparation & Execution
-$sql =
-  'SELECT
-      version,
-      date,
-      type,
-      notes
-   FROM
-      updates
-   ORDER BY
-      version Desc'
-;
-$sqlResult = mysqli_query($con, $sql);
+    return $arr;
+  })();
+  // Get Changelogs
+  (function () use (&$con, &$response, $params) {
+    $cols = $data = ['version', 'date', 'type', 'notes'];
+    $select = implode(', ', $cols);
+    $where =
+    $s = [
+      'select' => implode(', ', $cols),
+      'where'  => (function() use ($params) {
+                    $p = $params['hash'];
 
-// Add Updates to Response Object
-while($row = mysqli_fetch_array($sqlResult)) {
-  $update = new Update;
+                    if ($p) { return "WHERE version = '${p}' OR 1"; }
+                    else    { return ''; }
+                  })(),
+      'order' => (function () use ($params) {
+                    $p = $params['hash'];
 
-  foreach($info as $key => $value) {
-    $update->$value = $row[$value];
-  }
+                    if ($p) { return "CASE version WHEN '${p}' THEN 0 ELSE 1 END,"; }
+                    else    { return ''; }
+                  })()
+    ];
+    $sql = $con->prepare("SELECT
+                            ${s['select']}
+                          FROM
+                              updates
+                          ${s['where']}
+                          ORDER BY
+                            ${s['order']}
+                            version DESC
+                          LIMIT
+                            ${params['limit']}
+                          OFFSET
+                            ${params['offset']}");
 
-  $response->response[count($response->response)] = $update;
-}
+    if (!$con->error) {
+      $sql->execute();
+      $sql->store_result();
+      $sql->bind_result(...$data);
+      $response->addPayload([], 'changelogs');
 
-// Return Update Notes
-echo json_encode($response);
+      while($sql->fetch()) {
+        $arr = [];
+
+        foreach ($data as $key => $val) {
+          $v;
+
+          if ($key == 'date') {
+            $date = new DateTime($val);
+            $v = $date->format('c');
+          }
+          else {
+            $v = $val;
+          }
+
+          $arr[$key] = $val;
+        }
+        $response->payload['changelogs'][] = array_combine($cols, $arr);
+      }
+
+      if ($params['firstRun'] == 'true') {
+        $sql->prepare("SELECT
+                         version
+                       FROM
+                          updates
+                       ORDER BY
+                          version DESC");
+
+        if (!$con->error) {
+          $ver = '';
+          $sql->execute();
+          $sql->store_result();
+          $sql->bind_result($ver);
+          $response->addPayload([], 'versions');
+
+          while($sql->fetch()) {
+            $response->payload['versions'][] = $ver;
+          }
+        }
+        else {
+          $response->fatalError(3, [
+            'name' => 'MySQL Error',
+            'Error' => $con->error
+          ]);
+        }
+      }
+
+      $response->send();
+    }
+    else {
+      $response->fatalError(3, [
+        'name' => 'MySQL Error',
+        'Error' => $con->error
+      ]);
+    }
+  })();
 ?>
--- assets/scripts/files/local/updates.js
+++ assets/scripts/files/local/updates.js
@@ -1,192 +1,255 @@
-/*********************************
-  Updates Page Scripts
-*********************************/
+var changelogProps = {
+  limit: 10,
+  offset: 0,
+  hash: false,
+  firstRun: true
+};
+var changelogIdPrefix = 'changelog_';
 
-// *** Immediate Functions ***
-// Handles Changelogs' Construction
-(function () {
+function getChangelogs () {
+  let idPrefix = changelogIdPrefix;
+  //  ^ Prefix for changelog IDs
   let count = {
-    'retrieved': 0,
-    'total': 0
+    fetched: 0,
+    added: 0
   };
+  let query = (function () {
+    let str = '';
+    let propNames = Object.keys(changelogProps);
 
-  // Construct the Changelog Panel & add it to the page
-  function constructPanel (updateObject) {
-    let panel = {}; // Changelog Panel Elements
-      (function () {
-        panel.template = document.getElementById('panel_template');
-        panel.base = panel.template.content.children[0].cloneNode(true);
-        panel.header = panel.base.getElementsByClassName('header')[0];
-          panel.icon = panel.header.getElementsByClassName('icon')[0].getElementsByClassName('fas')[0];
-          panel.version = panel.header.getElementsByClassName('version')[0];
-          panel.date = panel.header.getElementsByClassName('info')[0].getElementsByClassName('date')[0];
-          panel.type = panel.header.getElementsByClassName('info')[0].getElementsByClassName('type')[0];
-        panel.body = panel.base.getElementsByClassName('body')[0];
-          panel.fullLink = panel.body.getElementsByClassName('full-changelog-link')[0].getElementsByTagName('a')[0];
-      })();
-    let ver = updateObject.version;
+    for (let i = 0; i < propNames.length; i++) {
+      let name = propNames[i];
+      let val = changelogProps[name];
 
-    // Handle Panel Properties
+      str += `&${name}=${val}`;
+    }
+
+    return str.replace('&', '?');
+  })();
+  let jump = getElement('updates_header_jump');
+  let list = document.getElementById('changelog_list');
+
+  function toggleOverlay (overlayIsHidden) {
+    let overlay = getElement('changelog_overlay');
+
+    if (overlayIsHidden) {
+      addClass(overlay, 'inactive');
+
+      setTimeout(function () {
+        isHidden(overlay, true);
+      }, 250);
+    }
+    else {
+      isHidden(overlay, false);
+
+      setTimeout(function () {
+        delClass(overlay, 'inactive');
+      }, 50);
+    }
+  }
+  function clearList () {
+    list.innerHTML = '';
+  }
+  function addChangelog(cl) {
+    let changelog = getTemplate('changelog_template');
+    let e = {
+      icon: getClass(changelog, 'icon').childNodes[0],
+      version: getClass(changelog, 'version'),
+      date: getClass(changelog, 'date'),
+      type: getClass(changelog, 'type'),
+      body: getClass(changelog, 'body')
+    };
+    let ver = cl.version;
+
+    // Properties
     (function () {
-      // Panel ID
-      panel.base.id = ('version_') + ver;
-      // Panel animation timing
-      panel.base.style.animationDelay = (count.total * 0.2) + 's';
+      changelog.id = idPrefix + ver;
+      changelog.style.animationDelay = `${(count.added * 0.2)}s`;
     })();
-    // Handle Header Properties
+    // Header
     (function () {
-      let type = updateObject.type;
       let icons = {
-        'Major': 'fa-broadcast-tower',
-        'Minor': 'fa-cogs',
-        'Patch': 'fa-tools'
+        major: 'fa-broadcast-tower',
+        minor: 'fa-cogs',
+        patch: 'fa-tools'
       };
-      let typeString = (function () {
-        if (type != 'Patch') { return type + (' Update'); }
-        else                 { return type; }
+      //  ^ Changelog Icons
+      let type = cl.type.toLowerCase();
+      let typeStr = (function () {
+        if (type == 'patch') { return cl.type; }
+        else                 { return `${cl.type} Update`; }
       })();
 
-      panel.icon.classList.add(icons[type]);
-      updateLabel(panel.icon, typeString);
-      panel.version.innerHTML = ('Version ') + updateObject.version;
-      panel.date.innerHTML = (function () {
-        let date = updateObject.date;
-        let y = date.substring(0, 4);
-        let md = date.substring(5);
+      addClass(e.icon, icons[type]);
+      updateLabel(e.icon, typeStr);
 
-        return md + ('-') + y;
-      })();
-      panel.type.innerHTML = typeString;
+      e.version.innerHTML = `Version ${ver}`;
+      e.date.innerHTML = datetime('month-date-year', cl.date);
+      e.type.innerHTML = typeStr;
     })();
-    // Handle Body Properties
+    // Body
     (function () {
-      let updateNotes = document.createElement('div');
+      let notes = (function () {
+        let regexps = {
+          // Format Sections
+          '(#{3}\\s{1})(?=\\w)': '</ul><h3>',
+          '\\s{1}#{3}': '</h3><ul class="styled">',
+          // Format Lists
+          '-.+': function (match) {
+            return match.replace(/-\s{1}/g, '<li>') + '</li>';
+          },
+          // Format Bolded Content
+          '\\*{2}(?=[\\w.])': '<strong>',
+          '\\*{2}(?![\\w.])': '</strong>',
+          // Format Emphasized Content
+          '_{1}(?=[\\w.])': '<em>',
+          '_{1}(?![\\w.])': '</em>',
+          // Format Code Content
+          '`{1}(?=[\\w.])': '<code>',
+          '`{1}(?![\\w.])': '</code>'
+        };
+        let exps = Object.keys(regexps);
+        let n = cl.notes;
 
-      updateNotes.className = 'update-notes';
-      updateNotes.innerHTML = (function () {
-        let notes = updateObject.notes;
+        for (let i = 0; i < exps.length; i++) {
+          let exp = exps[i];
+          let regex = new RegExp(exp, 'g');
+          let replacement = regexps[exp];
 
-        function updateChanges (match) { return match.replace(/-\s{1}/g, '<li>') + '</li>'; }
-
-        // Replace Markdown with HTML Markup
-        // Format Sections
-        notes = notes.replace(/(#{3}\s{1})(?=\w)/g, '</ul><h3>');
-        notes = notes.replace(/\s{1}#{3}/g, '</h3><ul class="styled">');
-        // Format Lists
-        notes = notes.replace(/-.*/g, updateChanges);
-        // Format Bolded Content
-        notes = notes.replace(/\*{2}(?=[\w.])/g, '<strong>');
-        notes = notes.replace(/\*{2}(?![\w.])/g, '</strong>');
-        // Format Emphasized Content
-        notes = notes.replace(/_{1}(?=[\w.])/g, '<em>');
-        notes = notes.replace(/_{1}(?![\w.])/g, '</em>');
-        // Format Code Content
-        notes = notes.replace(/`{1}(?=[\w.])/g, '<code>');
-        notes = notes.replace(/`{1}(?![\w.])/g, '</code>');
-        // Correct the final output
-        notes = notes.replace(/<\/ul>/, ''); // Remove First closing List tag
-        notes = notes + ('</ul>');           // Add Final closing List tag
-
-        return notes;
-      })();
-
-      panel.body.insertBefore(updateNotes, panel.body.childNodes[0]);
-      panel.fullLink.href = ('https://github.com/FusedKush/ShiftCodesTK/releases/tag/v') + ver;
-      updateLabel(panel.fullLink, ('Complete Release Notes for Version ') + ver + (' (External Link)'));
-    })();
-    // Handle Panel Event Listeners
-    (function () {
-      dropdownPanelSetup(panel.base);
-    })();
-    // Add panel to page
-    (function () {
-      let main = document.getElementsByTagName('main')[0];
-
-      main.appendChild(panel.base);
-      count.total++;
-
-      // Handle Header Updates
-      (function () {
-        let id = panel.base.id;
-        let title = (function () {
-          let start = id.slice(0, 1).toUpperCase();
-          let parsed = id.replace('_', ' ');
-
-          return start + parsed.slice(1);
-        })();
-        let current = document.getElementById('updates_header_current');
-        let dropdown = document.getElementById('updates_header_jump_dropdown');
-        let choiceList = dropdown.getElementsByClassName('choice-list')[0];
-
-        // Update Latest Update field
-        if (count.total == 1) {
-          let section = current.parentNode.parentNode;
-
-          current.href = ('#') + id;
-          current.getElementsByTagName('strong')[0].innerHTML = title.replace('Version ', '');
-          updateLabel(current, ('Jump to ') + title + (' Changelog'));
-
-          vishidden(section, false);
-          setTimeout(function () { section.removeAttribute('data-hidden'); }, 50);
+          n = n.replace(regex, replacement);
         }
 
-        // Add link to Jump Button
-        (function () {
-          let e = {};
-            (function () {
-              e.li = document.createElement('li');
-              e.a = document.createElement('a');
-              e.span = document.createElement('span');
-            })();
-
-          e.li.setAttribute('role', 'menuitem');
-          e.a.className = 'choice internal';
-          e.a.href = ('#') + id;
-          e.a.setAttribute('data-value', title);
-          e.span.innerHTML = title;
-          updateLabel(e.a, ('Jump to ') + title + (' Changelog'))
-
-          e.a.appendChild(e.span);
-          e.li.appendChild(e.a);
-          choiceList.appendChild(e.li);
-          choiceList.lastChild.addEventListener('click', function () {
-            toggleDropdownMenu(dropdown, true);
-          });
-        })();
+        n = n.replace(/<\/ul>/, ''); // Remove First closing List tag
+        n += '</ul>';                // Add Final closing List tag
+        return n;
       })();
-      if (count.retrieved == count.total) {
-        addFocusScrollListeners(main);
-        disenable(document.getElementById('updates_header_jump'), false);
-        panel.template.remove();
+
+      e.body.innerHTML = notes;
+    })();
+    // Listeners
+    dropdownPanelSetup(changelog);
+    // Add to page
+    (function () {
+      if (count.added == 0) {
+        clearList();
+      }
+
+      list.appendChild(changelog);
+      count.added++;
+
+      if (count.added == 1) {
+        toggleOverlay(true);
+      }
+      if (count.added == count.fetched) {
+        addFocusScrollListeners(list);
+        hashUpdate();
+
+        if (!changelogProps.firstRun) {
+          isDisabled(jump, false);
+        }
       }
     })();
   }
+  function handleResponse (serverResponse) {
+    let response = tryJSONParse(serverResponse);
 
-  // Retrieve updates and add them to the page
-  (function () {
-    // Wait for dependencies
-    function waitForDep() {
-      if (typeof newAjaxRequest == 'function') {
-        // Fetch changelogs
-        newAjaxRequest({
-          file: 'assets/php/scripts/getChangelogs.php',
-          callback: function (response) {
-            let changelogs = JSON.parse(response).response;
-                count.retrieved = changelogs.length;
+    if (response && response.statusCode == 0) {
+      let changelogs = response.payload.changelogs;
+          count.fetched = changelogs.length;
 
-            // Process changelogs
-            for (let i = 0; i < count.retrieved; i++) {
-              // Construct the panel
-              constructPanel(changelogs[i]);
-            }
-          }
-        });
+      for (let changelog of changelogs) {
+        addChangelog(changelog);
       }
-      else { setTimeout(function () { waitForDep(); }, 100); }
+
+      if (changelogProps.firstRun) {
+        let vers = response.payload.versions;
+        changelogProps.firstRun = false;
+
+        // Setup pager
+        (function () {
+          let pager = getElement('changelog_pager');
+
+          pager.setAttribute('data-max', Math.ceil((vers.length / changelogProps.limit)));
+          pager = configurePager(pager);
+          addPagerListeners(pager, function (e) {
+            let val = tryParseInt(this.getAttribute('data-value'));
+
+            if (val != changelogProps.offset) {
+              changelogProps.offset = val;
+              getChangelogs();
+            }
+          });
+        })();
+        // Setup Jump dropdown
+        (function () {
+          let jump, dropdown;
+          let template = document.getElementById('changelog_jump_template');
+
+          tryToRun({
+            function: function () {
+              jump = document.getElementById('updates_header_jump');
+              dropdown = document.getElementById('updates_header_jump_dropdown');
+
+              return true;
+            }
+          });
+
+          for (let ver of vers) {
+            let choice = getTemplate(template);
+            let link = getTag(choice, 'a');
+
+            link.href += idPrefix + ver;
+            updateLabel(link, link.href.replace('[\\d\\.]+', ver));
+            link.childNodes[0].innerHTML = `Version ${ver}`;
+
+            getClass(dropdown, 'choice-list').appendChild(choice);
+          }
+
+          setupDropdownMenu(dropdown);
+          isDisabled(jump, false);
+          deleteElm(template);
+        })();
+      }
+    }
+    else {
+      clearList();
+      toggleOverlay(false);
+      newToast({
+        settings: {
+          template: 'exception'
+        },
+        content: {
+          body: 'We could not retrieve the changelogs from the server at this time. Please try again later.'
+        }
+      });
     }
 
-    waitForDep();
-  })();
-})();
+    lpbUpdate(100);
+  }
 
-// *** Event Listeners ***
+  lpbUpdate(90, true, {start: 20});
+  isDisabled(jump, true);
+  newAjaxRequest({
+    file: `assets/php/scripts/getChangelogs${query}`,
+    callback: handleResponse
+  });
+}
+
+(function () {
+  let interval = setInterval(function () {
+    if (globalFunctionsReady && pagers) {
+      clearInterval(interval);
+
+      let currentHashState = addHashListener(changelogIdPrefix, function (hash) {
+        changelogProps.offset = 0;
+        changelogProps.hash = hash.replace(`#${changelogIdPrefix}`, '');
+        getChangelogs();
+        changelogProps.hash = false;
+      });
+
+      if (!currentHashState) {
+        getChangelogs();
+      }
+    }
+  }, 250);
+})();
