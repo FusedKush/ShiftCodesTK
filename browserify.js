ae5548b3a9e04d0c6efa6813513e61ee2a8f3ea4
Zach Vaughan <FusedKush@gmail.com>
2021-03-11 22:40:56 -0700

    Begin migration of `moment` to `date-fns`

--- browserify.js
+++ browserify.js
@@ -1,6 +1,7 @@
 /** @var Object.<string, string> The *Browserify Modules* to be loaded from the `node_modules` folder. */
 const BROWSERIFY_MODULES = {
-  moment: require('moment')
+  moment: require('moment'),
+  date_fns: require('date-fns')
 };
 
 module.exports = BROWSERIFY_MODULES;
