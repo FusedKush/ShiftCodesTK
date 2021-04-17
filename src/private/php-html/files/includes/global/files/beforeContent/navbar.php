91cf1594f5f5aa4a9ddbf1f660b87e275e880087
Zach Vaughan <FusedKush@gmail.com>
2021-04-16 23:53:40 -0600

    Update the *Account Menu* on the Navbar
    - Always show the *Account Menu*, instead of only when logged in.
    - Add *Login* and *Create Account* links when logged out.
    - Add the user's *Profile Picture* when logged in and set.
    - Allow the *Global Stats* link to appear when enabled even
    when logged out.

--- src/private/php-html/files/includes/global/files/beforeContent/navbar.php
+++ src/private/php-html/files/includes/global/files/beforeContent/navbar.php
@@ -33,130 +33,165 @@
           </div>
         </li>
       </div>
-      <?php if (CurrentUser::is_logged_in()) : ?>
-        <div class="right">
-          <li role="menuitem">
-            <button 
-            class="btn bubble-parent no-focus-scroll layer-target" 
-            id="navbar_account_menu" 
-            aria-pressed="false">
-              <span class="bubble bubble-dynamic"></span>
+      <div class="right">
+        <li role="menuitem">
+          <button 
+          class="btn bubble-parent no-focus-scroll layer-target" 
+          id="navbar_account_menu" 
+          aria-pressed="false">
+            <span class="bubble bubble-dynamic"></span>
+            <?php
+              $profile_picture = CurrentUser::is_logged_in()
+                ? CurrentUser::get_current_user(true)->profile_picture
+                : null;
+            ?>
+            <?php if (isset($profile_picture)) : ?>
+              <img 
+                alt="FusedKush's Profile Picture" 
+                src="/assets/img/users/profiles/149357043452/e266e1c897bb.jpg?_request_token=<?= $_SESSION['token']; ?>&size=128">
+            <?php else : ?>
               <span class="fas fa-user"></span>
-            </button>
-            <div class="layer tooltip sticky navbar-layer" data-layer-pos="bottom" data-layer-delay="long">
-              Open the Account Menu
+            <?php endif; ?>
+          </button>
+          <div class="layer tooltip sticky navbar-layer" data-layer-pos="bottom" data-layer-delay="long">
+            My Account
+          </div>
+          <div class="layer dropdown sticky navbar-layer auto-toggle" id="navbar_account_menu_dropdown" data-layer-pos="bottom">
+          <div class="title">
+          <?php if (CurrentUser::is_logged_in()) : ?>
+            <div 
+              class="profile-card" 
+              data-card-user="<?= Strings\encode_html(json_encode(CurrentUser::get_current_user()->get_profile_card_data())); ?>"
+              data-card-flags="CARD_HIDE_BORDER">
             </div>
-            <div class="layer dropdown sticky navbar-layer auto-toggle" id="navbar_account_menu_dropdown" data-layer-pos="bottom">
-              <div class="title">
-                <div 
-                  class="profile-card" 
-                  data-card-user="<?= Strings\encode_html(json_encode(CurrentUser::get_current_user()->get_profile_card_data())); ?>"
-                  data-card-flags="CARD_HIDE_BORDER">
+          <?php endif; ?>
+          </div>
+            <ul class="choice-list">
+            <!-- Global Stats Modal Toggle -->
+            <?php if (isset($_GET['show_global_function_stats'])) : ?>
+              <li>
+                <button
+                  class="choice layer-target modal-toggle auto-toggle"
+                  data-modal="global_function_stats_modal">
+                  <span 
+                    class="<?= "box-icon fas fa-clock"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">Function Stats</span>
+                </button>
+                <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
+                  Statistics related to the&nbsp;<code class="dark-bg">dom</code>&nbsp;&&nbsp;<code class="dark-bg">edit</code>&nbsp;helper functions
                 </div>
-              </div>
-              <ul class="choice-list">
-                <!-- New Code -->
-                <li>
-                  <a
-                    class="choice layer-target"
-                    href="/codes/new">
-                    <span 
-                      class="<?= "box-icon fas fa-plus"; ?>"
-                      aria-hidden="true">
-                    </span>
-                    <span class="label">New Code</span>
-                  </a>
-                  <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
-                    Submit a new SHiFT Code to ShiftCodesTK
-                  </div>
-                </li>
-                <!-- My Codes -->
-                <li>
-                  <a
-                    class="choice layer-target"
-                    href="/codes/">
-                    <span 
-                      class="<?= "box-icon fas fa-list-alt"; ?>"
-                      aria-hidden="true">
-                    </span>
-                    <span class="label">My Codes</span>
-                  </a>
-                  <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
-                    Your submitted SHiFT Codes
-                  </div>
-                </li>
-                <!-- Developer Playground -->
-                <?php
-                  $currentUser = \ShiftCodesTK\Users\CurrentUser::get_current_user();
-                    
-                  if ($currentUser && $currentUser->has_permission('DEVELOPER_TOOLS')) :
-                ?>
-                  <div class="separator" aria-hidden="true"></div>
-                  <li>
-                    <a
-                      class="choice layer-target"
-                      href="/dev/playground">
-                      <span 
-                        class="<?= "box-icon fas fa-laptop-code"; ?>"
-                        aria-hidden="true">
-                      </span>
-                      <span class="label">Dev Playground</span>
-                    </a>
-                    <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
-                      The&nbsp;<em>Developer Playground</em>&nbsp;is a testing ground for element styles & functionality.
-                    </div>
-                  </li>
-                <?php endif; ?>
-                <!-- End of `DEVELOPER_TOOLS` permission check -->
-
-                <!-- Global Stats Modal Toggle -->
-                <?php if (isset($_GET['show_global_function_stats'])) : ?>
-                  <div class="separator" aria-hidden="true"></div>
-                  <li>
-                    <button
-                      class="choice layer-target modal-toggle auto-toggle"
-                      data-modal="global_function_stats_modal">
-                      <span 
-                        class="<?= "box-icon fas fa-clock"; ?>"
-                        aria-hidden="true">
-                      </span>
-                      <span class="label">Function Stats</span>
-                    </button>
-                    <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
-                      Statistics related to the&nbsp;<code>dom</code>&nbsp;&&nbsp;<code>edit</code>&nbsp;helper functions
-                    </div>
-                  </li>
-                <?php endif; ?>
-
+              </li>
+              <div class="separator" aria-hidden="true"></div>
+            <?php endif; ?>
+            <?php if (CurrentUser::is_logged_in()) : ?>
+              <!-- New Code -->
+              <li>
+                <a
+                  class="choice layer-target"
+                  href="/codes/new">
+                  <span 
+                    class="<?= "box-icon fas fa-plus"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">New Code</span>
+                </a>
+                <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
+                  Submit a new SHiFT Code to ShiftCodesTK
+                </div>
+              </li>
+              <!-- My Codes -->
+              <li>
+                <a
+                  class="choice layer-target"
+                  href="/codes/">
+                  <span 
+                    class="<?= "box-icon fas fa-list-alt"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">My Codes</span>
+                </a>
+                <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
+                  Your submitted SHiFT Codes
+                </div>
+              </li>
+              <!-- Developer Playground -->
+              <?php
+                $currentUser = \ShiftCodesTK\Users\CurrentUser::get_current_user();
+                  
+                if ($currentUser && $currentUser->has_permission('DEVELOPER_TOOLS')) :
+              ?>
                 <div class="separator" aria-hidden="true"></div>
-                <!-- My Account -->
                 <li>
                   <a
                     class="choice layer-target"
-                    href="/account/">
+                    href="/dev/playground">
                     <span 
-                      class="<?= "box-icon fas fa-user"; ?>"
+                      class="<?= "box-icon fas fa-laptop-code"; ?>"
                       aria-hidden="true">
                     </span>
-                    <span class="label">My Account</span>
+                    <span class="label">Dev Playground</span>
                   </a>
                   <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
-                    View and manage your ShiftCodesTK Account
+                    The&nbsp;<em>Developer Playground</em>&nbsp;is a testing ground for element styles & functionality.
                   </div>
                 </li>
-                <!-- Logout Button -->
-                <li>
-                  <?php
-                    include(\ShiftCodesTK\Paths\PHP_PATHS['forms'] . '/auth/logout.php');
+              <?php endif; ?>
+              <!-- End of `DEVELOPER_TOOLS` permission check -->
 
-                    $form_authLogout->insertForm();
-                  ?>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </div>
-      <?php endif; ?>
+              <div class="separator" aria-hidden="true"></div>
+              <!-- My Account -->
+              <li>
+                <a
+                  class="choice layer-target"
+                  href="/account/">
+                  <span 
+                    class="<?= "box-icon fas fa-user"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">My Account</span>
+                </a>
+                <div class="layer tooltip" data-layer-pos="left" data-layer-delay="medium">
+                  View and manage your ShiftCodesTK Account
+                </div>
+              </li>
+              <!-- Logout Button -->
+              <li>
+                <?php
+                  include(\ShiftCodesTK\Paths\PHP_PATHS['forms'] . '/auth/logout.php');
+
+                  $form_authLogout->insertForm();
+                ?>
+              </li>
+            <?php else : ?>
+              <!-- Login Button -->
+              <li>
+                <a
+                  class="choice"
+                  href="/account/login">
+                  <span 
+                    class="<?= "box-icon fas fa-sign-in-alt"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">Login</span>
+                </a>
+              </li>
+              <!-- Create Account Button -->
+              <li>
+                <button class="choice layer-target" disabled>
+                  <span 
+                    class="<?= "box-icon fas fa-user-plus"; ?>"
+                    aria-hidden="true">
+                  </span>
+                  <span class="label">Create Account</span>
+                </a>
+              </li>
+            <?php endif; ?>
+            </ul>
+          </div>
+        </li>
+      </div>
     </ul>
   </nav>
 </header>
