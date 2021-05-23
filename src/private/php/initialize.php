37f26d70e84c1baf1a3279e74f57217dfdd05162
Zach Vaughan <FusedKush@gmail.com>
2021-05-23 10:16:15 -0600

    Update behavior of initialization variables
    Updated the behavior of variables defined in the global scope during
    initialization, defining a `t_` prefix to help designate these temporary
    variables.

--- src/private/php/initialize.php
+++ src/private/php/initialize.php
@@ -1,18 +1,27 @@
 <?php
   /** 
-   * The primary PHP Initialization File. All PHP Scripts *must* include this file at least once. 
+   * The primary PHP Initialization File.
+   * 
+   * Any variables defined during initialization must **not** be in the 
+   * *Global Space* once initialization has completed. 
+   * - Where possible, wrap initialization code and variables in an *Anonymous Function*.
+   * - If an Anonymous Function is not viable, the variables **must** be prefixed by `t_` and
+   * *unset* once completed. This is typically the case when working with *PHP Includes*.
    **/
 
   // Script Initialization
-  $__initialization_files = [
+  $t_init_files = [
     'definition-constants',
     'loaders',
     'startup'
   ];
 
-  foreach ($__initialization_files as $filename) {
-    include(__DIR__ . "/initialize/{$filename}.php");
+  foreach ($t_init_files as $t_init_file) {
+    include(__DIR__ . "/initialize/{$t_init_file}.php");
   }
 
-  unset($__initialization_files);
+  unset(
+    $t_init_files, 
+    $t_init_file
+  );
 ?>
